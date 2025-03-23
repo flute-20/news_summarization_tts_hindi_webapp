1af8041126e969eacdd8c712827648312f9f004b
flute-20 <vijayasrisangepu@gmail.com>
2025-03-23 16:28:30 +0530

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -28,4 +28,4 @@ def get_news():
     
 #
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , port = int(os.getenv("PORT" , 5000)) , debug = False )
+    app.run(host='0.0.0.0' , port = int(os.getenv("PORT" , 10000)) , debug = False )
